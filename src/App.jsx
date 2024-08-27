0e4b8f1d38afdc29e00a15dd37c07261ef45a5bb
S-Anbu <anbarasans750@gmail.com>
2024-08-27 14:21:07 +0530

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>QR CODE GENERATOR</h1>
+    <div className='wraper'>
+      <label htmlFor="userdata">User data</label>
+      <input type="text"  id='userdata' />
+      <label htmlFor="userdata">QR Code size</label>
+      <input type="text" id='userdata' />
+    </div>
+    <dir>
+      <button className='button1' type="button">Generate QR</button>
+      <button className='button2' type="button">Download QR</button>
+    </dir>
     </>
   )
 }
